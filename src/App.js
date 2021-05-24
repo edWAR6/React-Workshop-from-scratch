51a32d7c237ba9524ee37a7fabefbaebe401f80e
Eduardo Oviedo Blanco <eduardo.oviedo@gmail.com>
2021-05-24 15:07:31 -0600

    Day two of the workshop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+import { hot } from 'react-hot-loader';
+import TodoList from './components/todos/TodoList';
 
 const App = () =>(
   <div className="App">
-    <h1>Hello World!!</h1>
+    <TodoList />
   </div>
 );
 
-export default App;
+export default hot(module)(App);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import App from './App';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from './store/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
